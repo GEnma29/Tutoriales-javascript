b91739dde2548e6a7977d971a6ff5dde6752b2d5
Enmanuel Alfonzo Angulo <53823068+GEnma29@users.noreply.github.com>
2021-01-14 17:43:53 -0400

    Create promise

---
+++ Promise/StructurePromise.js
@@ -0,0 +1,35 @@
+const somethingWillHappen = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            resolve('Hey!');
+        } else{
+            reject('Whooops!');
+        }
+    });
+};
+somethingWillHappen()
+   .then(response => console.log(response))
+   .catch(err => console.error(err));
+
+const somethingWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            resolve('Hey!');
+        } else{
+            reject('Whooops!');
+        }
+    });
+};
+
+somethingWillHappen2()
+   .then(response => console.log(response))
+   .catch(err => console.error(err));
+
+
+   Promise.all([somethingWillHappen(), somethingWillHappen2()])
+     .then(response => {
+         console.log('Array of results', response);
+     })
+     .catch(err => {
+         console.error(err);
+     })
